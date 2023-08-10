551dd3184972632537a468b1249793da534196de
awiseguy <arvin@wise-it.info>
2023-08-10 17:59:43 +0200

    hello world

---
+++ 1.3.0/go/goto.js
@@ -0,0 +1,3 @@
+VoegRepositoryToe(
+[{"Prefix": "stop", "Url": "https://koop.gitlab.io/STOP/team/test-1.0"}]
+);
---
+++ 1.3.0/versieinfo.js
@@ -0,0 +1,9 @@
+var VersieInfo_Versie = '1.3.0';
+
+VersieInfo_VersieTekst = function () {
+    return 'versie ' + VersieInfo_Versie;
+}
+
+VersieInfo_VersieActualiteit = function () {
+    return '';
+}
